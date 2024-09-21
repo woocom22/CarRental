f505dc14cb076be2fbd480befc51e0e6d98045fc
Debasish roy <dkroy2010@outlook.com>
2024-09-21 22:08:30 +0600

    Chenge user, car and rental model

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Rental extends Model
 {
@@ -11,6 +12,20 @@ class Rental extends Model
     protected $fillable = [
         'start_date',
         'end_date',
-        'total_cost'
+        'total_cost',
+        'user_id',
+        'car_id'
     ];
+
+    public function car():belongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function user():belongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
